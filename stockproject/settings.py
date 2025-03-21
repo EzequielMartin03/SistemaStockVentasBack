41159c179d51326c38f17f3df8463ad48b3cd23c
ezequielmartin03 <martinezequielnicolas@gmail.com>
2025-03-20 22:14:48 -0300

    arreglos

--- stockproject/settings.py
+++ stockproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stockventas',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
