781b0fafd23facaa918b9c1866fdc3a74135c93e
ezequielmartin03 <martinezequielnicolas@gmail.com>
2025-03-26 10:34:55 -0300

    26/03 arreglos

--- stockventas/views.py
+++ stockventas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 
@@ -9,19 +9,16 @@ def login(request):
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST['user']
+        username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
+            auth_login(request, user)
             return redirect('index')  # O la URL que desees
         else:
             return render(request, 'login.html', {'error': 'Credenciales incorrectas'})
     return render(request, 'login.html')
 
-
-def login(request):
-    return render(request, 'login.html')
 
 
 def logout(request):
